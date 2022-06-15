bd7bb1bdd9a31c234073fabaa73c064b9e2ed93b
regal-rahul <regalrahul777@gmail.com>
2022-06-15 21:49:58 +0530

    Set viewengine

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const User = require('./models/user');
 
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.get('/secret', (req, res) => {
+    res.send('This is Secret!');
+})
+
+
 app.listen(3000, () => {
     console.log('Listening to port 3000');
 });
