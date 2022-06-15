24d8930a7c3b8f59718d5f1250e1909abf3bb536
regal-rahul <regalrahul777@gmail.com>
2022-06-15 21:44:47 +0530

    Added basic user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username cannot be blank']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password cannot be empty']
+    }
+})
